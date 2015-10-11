9e4fd9019dd9cf6991435370c9179b72a187bedf
rageandqq <chitley.sameer@gmail.com>
2015-10-10 17:57:03 -0700

    Updated app.py to read N points from file and query then dump

--- app.py
+++ app.py
@@ -1,58 +1,96 @@
 import aerospike
 import urllib2
 import json
+import sys
+import datetime
+
+args = sys.argv
+FILE_NAME = args[1]
+NUM_POINTS = int(args[2])
+NAMESPACE = 'prod'
+SET = 'price'
+
+#######################
+# Read points from file
+#######################
+
+with open(FILE_NAME, 'r') as ins:
+    lines = []
+    for line in ins:
+        strs = line.split()
+        ll_tuple = (strs[0], strs[1])
+        lines.append(ll_tuple)
+
 
 ##############################
 ## Set up aerospike connection
 ##############################
-#config = {
-#    'hosts': [
-#        ( '54.152.234.234', 3000, )
-#    ],
-#    'policies': {
-#        'timeout': 1000, # milliseconds
-#    }
-#}
-#
-#client = aerospike.client(config)
-#client.connect()
+config = {
+    'hosts': [
+        ( '54.152.234.234', 3000, )
+    ],
+    'policies': {
+        'timeout': 1000, # milliseconds
+    }
+}
+
+client = aerospike.client(config)
+client.connect()
 
 ###########
 # Query Uber
 ###########
 
-server_token = 'fEdujyqYJcU7IM-FjxjDOH6Vc2Y1RAxrB7IVhoBt'
+SERVER_TOKEN = 'fEdujyqYJcU7IM-FjxjDOH6Vc2Y1RAxrB7IVhoBt'
+data_list = []
 
-latitude = 37.871210
-longitude = -122.250281
-lat2 = 37.774183
-long2 = -122.394756
+# Unused requests
+#request_product = urllib2.Request(
+#        'http://api.uber.com/v1/products?latitude={}&longitude={}'.
+#            format(latitude, longitude),
+#        )
+#
+#
+#request_time = urllib2.Request(
+#        'http://api.uber.com/v1/estimates/time?start_latitude={}&start_longitude={}'.
+#            format(latitude, longitude),
+#        )
 
-request_product = urllib2.Request(
-        'http://api.uber.com/v1/products?latitude={}&longitude={}'.
-            format(latitude, longitude),
-        )
-
-
-request_time = urllib2.Request(
-        'http://api.uber.com/v1/estimates/time?start_latitude={}&start_longitude={}'.
-            format(latitude, longitude),
-        )
-
-
-
-request_price = urllib2.Request('https://api.uber.com/v1/estimates/price?start_latitude={}&start_longitude={}&end_latitude={}&end_longitude={}'.
-            format(
-                latitude,
-                longitude,
-                lat2,
-                long2,
+for i in range(NUM_POINTS):
+    latitude, longitude = lines[i]
+    request_price = urllib2.Request('https://api.uber.com/v1/estimates/price?start_latitude={}&start_longitude={}&end_latitude={}&end_longitude={}'.
+                format(
+                    latitude,
+                    longitude,
+                    latitude,
+                    longitude,
+                )
             )
-        )
 
-request_price.add_header('Authorization', 'Token %s' % server_token)
+    request_price.add_header('Authorization', 'Token %s' % SERVER_TOKEN)
 
-resp = urllib2.urlopen(request_price)
-data = json.load(resp)
+    resp = urllib2.urlopen(request_price)
+    data = json.load(resp)
 
-print json.dumps(data, indent=4, sort_keys=True)
+    data['latitude'] = latitude
+    data['longitude'] = longitude
+
+    data_list.append(data)
+
+##############
+# Write to DB
+##############
+
+epoch_time = (datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds()
+key = ('test', 'price', str(epoch_time),)
+
+stored_data = {
+    'timestamp': str(epoch_time),
+    'data': data_list,
+}
+client.put(key, stored_data)
+
+##############
+# Log to console
+##############
+print json.dumps(stored_data, indent=4, sort_keys=False)
